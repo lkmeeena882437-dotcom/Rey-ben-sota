a33e95e030446a050263b8d9b6bf3a6df03f1b29
lkmeeena882437-dotcom <lkmeeena882437@gmail.com>
2026-06-05 14:02:51 +0530

    Create NotFound component for 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+  return (
+    <main
+      style={{
+        minHeight: "60vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>404</h1>
+      <p>Product not found.</p>
+    </main>
+  );
+}
