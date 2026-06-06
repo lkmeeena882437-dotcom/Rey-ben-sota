89893de8b65d86975b691649458db44715a54a9a
lkmeeena882437-dotcom <lkmeeena882437@gmail.com>
2026-06-06 08:55:46 +0530

    Add productImages for rys-vision-pro

---
+++ lib/product-images.ts
@@ -0,0 +1,8 @@
+export const productImages = {
+  "rys-vision-pro": [
+    "https://images.unsplash.com/photo-1511499767150-a48a237f0083",
+    "https://images.unsplash.com/photo-1572635196237-14b3f281503f",
+    "https://images.unsplash.com/photo-1517841905240-472988babdf9",
+    "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7",
+  ],
+};
