45d666171ae0a3a1745634242768afc514bc3cff
lkmeeena882437-dotcom <lkmeeena882437@gmail.com>
2026-06-06 08:35:33 +0530

    Add FAQ and Newsletter sections to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import TrustBadges from "@/components/trust/TrustBadges";
 import ProductGrid from "@/components/product/ProductGrid";
 import CollectionGrid from "@/components/collections/CollectionGrid";
 import ReviewsSection from "@/components/reviews/ReviewsSection";
+import FAQSection from "@/components/faq/FAQSection";
+import NewsletterSection from "@/components/newsletter/NewsletterSection";
 import { products } from "@/lib/products";
 
 export default function HomePage() {
@@ -13,6 +15,8 @@ export default function HomePage() {
       <ProductGrid products={products} />
       <CollectionGrid />
       <ReviewsSection />
+      <FAQSection />
+      <NewsletterSection />
     </main>
   );
 }
